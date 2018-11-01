20a3f04a44588ef325a0068b2eea1de91fa830dc
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2018-11-01 14:26:03 +0200

    add create parcel delivery order finishes[#161573141]

---
+++ UI/script/main.js
@@ -0,0 +1,8 @@
+function open_orders(){
+    window.location.href='createOrder.html';
+}
+function confirm_request(){
+    if(window.confirm("This order might cost you some money, Are you sure you want to continue?")){
+        alert("Your Request has been saved");
+    }
+}
