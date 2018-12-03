77199e1ff5b34adda27baa6b40dcba46a745555b
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2018-12-03 06:49:31 +0200

    add login and signup javascript file

--- UI/script/main.js
+++ UI/script/main.js
@@ -58,29 +58,7 @@ function open_profile(){
     
 // }
 
-function redirect_user(){
-    if(document.getElementById('email').value ==="admin@admin.com" ){
-        alert('logging in as admin...')
-        window.location.href='orders.html';
-    }
-    else if(document.getElementById('email').value ==="" || document.getElementById('password').value===""){
-        alert("All fields have to be filled!");
-        
-    }
-    else{
-        alert('logging in as user...')
-        window.location.href='orders.html';
-    }
-}
 
-function confirm_password(){
-    if(document.getElementById('confirmpassword').value !==document.getElementById('password').value ){
-        alert('Password mismatch');
-    }
-    else if(document.getElementById('email').value ==="" || document.getElementById('password').value===""  || document.getElementById('confirmpassword').value){
-        alert("All fields have to be filled!");
-        
-    }
-}
+
 
 
